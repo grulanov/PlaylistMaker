cdd38da538f780dc9d83f2e44cd6c16a6f1e89f0
Grigory Ulanov <grigory.ulanov@yandex.ru>
2024-09-08 17:22:38 +0300

    Add main activity actions

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        searchButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Search button did tap", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Media button did tap", Toast.LENGTH_SHORT).show()
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Settings button did tap", Toast.LENGTH_SHORT).show()
         }
     }
 }
