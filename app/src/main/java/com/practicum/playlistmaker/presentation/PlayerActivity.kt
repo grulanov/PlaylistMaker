d629b61868e437534f179288984f7a96b62b4607
Grigory Ulanov <grigory.ulanov@yandex.ru>
2024-11-19 23:05:06 +0200

    Add player layout

--- app/src/main/java/com/practicum/playlistmaker/presentation/PlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/PlayerActivity.kt
@@ -3,8 +3,12 @@ package com.practicum.playlistmaker.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityPlayerBinding
 import com.practicum.playlistmaker.logic.domainModels.Track
@@ -33,6 +37,24 @@ class PlayerActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+
+        binding.trackNameTextView.text = track.trackName ?: "-"
+        binding.artistNameTextView.text = track.artistName ?: "-"
+        binding.trackTimeProgressTextView.text = "0:30" // TODO
+        binding.trackTimeValueTextView.text = track.trackTime ?: "-"
+        binding.yearValueTextView.text = track.releaseYear?.toString() ?: "-"
+        binding.genreValueTextView.text = track.primaryGenreName ?: "-"
+        binding.countryValueTextView.text = track.country ?: "-"
+
+        binding.collectionValueTextView.text = track.collectionName
+        binding.collectionTitleTextView.isVisible = track.collectionName != null
+        binding.collectionValueTextView.isVisible = track.collectionName != null
+
+        Glide.with(binding.root)
+            .load(track.artworkUrl500?.toUri())
+            .placeholder(R.drawable.placeholder_player_artwork)
+            .transform(RoundedCorners(resources.getDimensionPixelSize(R.dimen.corner_radius_s)))
+            .into(binding.trackImageView)
     }
 
     override fun onSupportNavigateUp(): Boolean {
